86f9ad9de835fbb3bc9a1f27f0eca8b77b0405ac
Nurlan Eliyev <nurlanali246@gmail.com>
2025-10-18 10:12:09 +0400

    first commit

---
+++ src/L3T3/task3.java
@@ -0,0 +1,19 @@
+package L3T3;
+
+public class task3 {
+    /// alinmadi
+    /// int[][][] cube = {
+    /// {1, 2}, {3, 4}
+    /// {5, 6}, {7, 8}
+    /// Gözlənilən çıxış:
+    /// 6-dan böyük ədədlər: 7, 8
+//    public static void main(String[] args) {
+//        int[][][] cube = {{{1, 2}, {3, 4}, {5, 6}, {7, 8}}};
+//        for (int i = 0; i < cube.length; i++) {
+//            if (cube[i] > 6) {
+//
+//            }
+//        }
+//    }
+}
+
