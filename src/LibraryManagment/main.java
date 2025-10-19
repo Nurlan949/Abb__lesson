c49abb2eb6ec16b415ef08823a37b74239800e4a
Nurlan Eliyev <nurlanali246@gmail.com>
2025-10-19 15:56:45 +0400

    commit

--- src/LibraryManagment/main.java
+++ src/LibraryManagment/main.java
@@ -3,18 +3,12 @@ package LibraryManagment;
 public class main {
     public static void main(String[] args) {
         library library = new library("Clean Code", "Robert C.Martin", "12345678");
-        library library2 = new library("Clean ", "Martin", "1115151");
+
         library.printInfo();
         library.borrowBook();
         library.returnBook();
         library.libraryRules();
         library.bookType();
-        System.out.println("-----------------------------");
-        System.out.println("-----------------------------");
-        library2.printInfo();
-        library2.borrowBook();
-        library2.returnBook();
-        library2.libraryRules();
-        library2.bookType();
+
     }
 }
