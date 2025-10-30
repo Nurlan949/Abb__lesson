1419a75e4178800c53beac8f4bc836b327598f1e
Nurlan Eliyev <nurlanali246@gmail.com>
2025-10-30 17:38:12 +0400

    commit

--- src/libraryManagement/Main.java
+++ src/libraryManagement/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Library<Library.Section> library = new Library<>();
         Library.Section fictionSection = new Library.Section("Fiction");
         Library.Section historySection = new Library.Section("History");
-        Library<Library.Section>.Book book1 = library.new Book("1984", "Nurlan Aiyev", 1949);
+        Library<Library.Section>.Book book1 = library.new Book("Tired", "Nurlan Aiyev", 1949);
         Library<Library.Section>.Book book2 = library.new Book("Happiness", "Rasul Kerimov", 1960);
         Library<Library.Section>.Book book3 = library.new Book("The Martian", "Andy Weir", 2011);
         fictionSection.addBook(book1);
