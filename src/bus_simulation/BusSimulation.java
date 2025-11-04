c2ccb458017c5b84d4aa3137f868750dceaa1f02
Nurlan Eliyev <nurlanali246@gmail.com>
2025-11-04 20:23:19 +0400

    commit

--- src/bus_simulation/BusSimulation.java
+++ src/bus_simulation/BusSimulation.java
@@ -2,7 +2,7 @@ package bus_simulation;
 
 import java.util.*;
 
-public class BusSimulation {///
+public class BusSimulation {//
     public static void main(String[] args) {//
         Random random = new Random();
         List<Queue<Passenger>> stop = new ArrayList<>();
@@ -36,6 +36,7 @@ public class BusSimulation {///
             }
             System.out.println("simulation finish");
             System.out.println("final passenger"+bus);
+
         }
 
 
